a1c25ac3f33891953297b9006590fcb9ca0a3227
David Zamora <david.zamora@sthaler.com>
2020-10-02 17:36:46 +0100

    feat(BurgerBuilder): build ingredients with state

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -4,12 +4,27 @@ import React from "react";
 import styles from "./Burger.module.scss";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
-const burger: FunctionComponent<{}> = (props) => {
+interface BurgerProps {
+  ingredients: { [key: string]: number };
+}
+
+const burger: FunctionComponent<BurgerProps> = (props) => {
+  let ingredientsJsx = Object.keys(props.ingredients)
+    .map((ingKey: string) => {
+      return [...Array(props.ingredients[ingKey])].map((_, i: number) => {
+        return <BurgerIngredient key={ingKey + i} type={ingKey} />;
+      });
+    })
+    .reduce((arr: JSX.Element[], el: JSX.Element[]) => arr.concat(el), []);
+
+  if (ingredientsJsx.length === 0) {
+    ingredientsJsx = [<p key="0">Please start adding ingredients!</p>];
+  }
+
   return (
     <div className={styles.Burger}>
       <BurgerIngredient type="bread-top"></BurgerIngredient>
-      <BurgerIngredient type="cheese"></BurgerIngredient>
-      <BurgerIngredient type="meat"></BurgerIngredient>
+      {ingredientsJsx}
       <BurgerIngredient type="bread-bottom"></BurgerIngredient>
     </div>
   );
