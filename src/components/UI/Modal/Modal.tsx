8fdf6181d6121eb26cde67f494be59a574264c98
David Zamora <david.zamora@sthaler.com>
2020-10-08 22:22:10 +0100

    ref(modal): react memo

--- src/components/UI/Modal/Modal.tsx
+++ src/components/UI/Modal/Modal.tsx
@@ -6,21 +6,24 @@ import Backdrop from "../Backdrop/Backdrop";
 interface ModalProps {
   show: boolean;
   modalClosed: Function;
+  children: React.ReactNode;
 }
 
-const modal: React.FunctionComponent<ModalProps> = (props) => (
-  <Aux>
-    <Backdrop show={props.show} clicked={props.modalClosed}/>
-    <div
-      style={{
-        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-        opacity: props.show ? "1" : "0",
-      }}
-      className={styles.Modal}
-    >
-      {props.children}
-    </div>
-  </Aux>
-);
+const modal: React.FunctionComponent<ModalProps> = (props) => {
+  return (
+    <Aux>
+      <Backdrop show={props.show} clicked={props.modalClosed} />
+      <div
+        style={{
+          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
+          opacity: props.show ? "1" : "0",
+        }}
+        className={styles.Modal}
+      >
+        {props.children}
+      </div>
+    </Aux>
+  );
+}
 
-export default modal;
+export default React.memo(modal, (prevProps, nextProps) => prevProps.show === nextProps.show);
