8cdd52c90670cef504f7c2071b4382015842b1a8
David Zamora <david.zamora@sthaler.com>
2020-10-08 19:40:16 +0100

    feat(Layout): added toolbar and sidedrawer

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Aux from "../../hoc/Auxiliary";
-import styles from './Layout.module.scss';
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import Sidedrawer from "../Navigation/Sidedrawer/Sidedrawer";
+import styles from "./Layout.module.scss";
 
 const layout: React.FunctionComponent<{}> = (props) => (
   <Aux>
-    <div className={styles.Content}>Toolbar, Sidedrawer, Backdrop</div>
-    <main>{props.children}</main>
+    <Toolbar />
+    <Sidedrawer />
+    <main className={styles.Content}>{props.children}</main>
   </Aux>
 );
 
